cb4da129cb0a942abc97cb3856d8e9e4e7520c64
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-03-05 14:44:44 +0100

    Add prod config for build

---
+++ webpack.prod.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+
+module.exports = {
+    devtool: 'source-map',
+    cache: false,
+    debug: false,
+
+    entry: {
+        app: './src/boot.ts',
+        vendors: [
+            'es6-shim',
+            'reflect-metadata',
+            'rxjs', // Agresiv and stupid
+            'zone.js/dist/zone-microtask',
+            'zone.js/dist/long-stack-trace-zone',
+            'angular2/platform/browser',
+            'angular2/core',
+            'angular2/http',
+            'angular2/router'
+        ]
+    },
+    output: {
+        path: './src/',
+        filename: 'one-file-to-rule-them-all.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.ts$/,
+            loader: 'ts',
+            exclude: /node_modules/,
+            query: {
+                compilerOptions: {
+                    removeComments: true,
+                    noEmitHelpers: true,
+                }
+            }
+        }, {
+            test: /\.css$/,
+            loader: 'style!css'
+        }],
+        noParse: [/zone\.js\/dist\/.+/]
+    },
+    resolve: {
+        cache: false,
+        extensions: ['', '.ts', '.js']
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js', Infinity),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(true),
+        new webpack.ProvidePlugin({
+            '__metadata': 'ts-helper/metadata',
+            '__decorate': 'ts-helper/decorate',
+            '__awaiter': 'ts-helper/awaiter',
+            '__extends': 'ts-helper/extends',
+            '__param': 'ts-helper/param'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+            mangle: false,
+            compress: { screw_ie8 : true },
+            comments: false
+        })
+    ]
+};
